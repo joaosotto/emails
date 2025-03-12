7bb3f63b2a1ff7eec3935cadc6875bcc781f672c
joaosotto <joao.maior@sou.inteli.edu.br>
2025-03-12 19:48:06 -0300

    specify route

--- src/discord/discord.controller.ts
+++ src/discord/discord.controller.ts
@@ -1,11 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { DiscordNotifier } from './discord.notifier';
 
-@Controller()
+@Controller('discord')
 export class DiscordController {
   constructor(private readonly discordNotifier: DiscordNotifier) {}
 
-  @Post()
+  @Post('send')
   async createNotifyRequest(@Body() message: string): Promise<void> {
     return await this.discordNotifier.sendMessage(message);
   }
